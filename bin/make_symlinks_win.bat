79c3c51f2fb3d1e23f867868ae4f94bdc65c44d8
uyorum <uyorum.pub@gmail.com>
2015-11-22 10:34:16 +0900

    bin: Add some symlinks on windows

--- bin/make_symlinks_win.bat
+++ bin/make_symlinks_win.bat
@@ -2,6 +2,7 @@
 
 mklink /D %HOME%\.emacs.d %HOME%\dotfiles\.emacs.d
 mklink /D %HOME%\.vim %HOME%\dotfiles\.vim
+mklink /D %HOME%\.zsh %HOME%\dotfiles\.zsh
 mklink %HOME%\.gemrc %HOME%\dotfiles\.gemrc
 mklink %HOME%\.gitconfig %HOME%\dotfiles\.gitconfig
 mklink %HOME%\.tmux.conf %HOME%\dotfiles\.tmux.conf
@@ -9,4 +10,4 @@ mklink %HOME%\.vimperatorrc %HOME%\dotfiles\.vimperatorrc
 mklink %HOME%\.vimrc %HOME%\dotfiles\.vimrc
 mklink %HOME%\.zshenv %HOME%\dotfiles\.zshenv
 mklink %HOME%\.zshrc %HOME%\dotfiles\.zshrc
-mklink %HOME%\Documents\AutoHotkey.ahk %HOME%\dotfiles\.windows\AutoHotkey.ahk
+mklink %USERPROFILE%\Documents\AutoHotkey.ahk %HOME%\dotfiles\.windows\AutoHotkey.ahk
