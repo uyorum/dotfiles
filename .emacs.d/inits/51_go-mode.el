a31cd3623da21adc68684ba8127cfbf135d95162
uyorum <uyorum.pub@gmail.com>
2015-11-23 04:47:36 +0900

    Emacs: settings for go-mode

--- .emacs.d/inits/51_go-mode.el
+++ .emacs.d/inits/51_go-mode.el
@@ -4,9 +4,16 @@
 ;; `go get -u github.com/nsf/gocode`
 (el-get-bundle! go-autocomplete)
 
+(add-hook 'go-mode-hook
+          '(lambda()
+             (setq c-basic-offset 4)
+             (setq indent-tabs-mode t)
+             (local-set-key (kbd "M-.") 'godef-jump)
+             (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
+             (local-set-key (kbd "C-c i") 'go-goto-imports)
+             (local-set-key (kbd "C-c d") 'godoc)))
+
 ;; run gofmt before save
 (add-hook 'before-save-hook 'gofmt-before-save)
-
 ;; auto import packages
 (setq gofmt-command "goimports")
-(add-hook 'before-save-hook 'gofmt-before-save)
