b99b8db3f8d3ce4278dc9df537e247496991fcbc
uyorum <uyorum.pub@gmail.com>
2015-12-06 13:22:41 +0900

    Emacs: using solarized theme

--- .emacs.d/inits/04_appearance.el
+++ .emacs.d/inits/04_appearance.el
@@ -28,56 +28,26 @@
 ;; (cancel-timer global-hl-line-timer)
 
 ;; フォントの設定
-(set-face-attribute 'default nil :font "源ノ角ゴシック Code JP R" :height 70)
-(add-to-list 'default-frame-alist '(font . "源ノ角ゴシック Code JP R-10"))
+(set-face-attribute 'default nil :font "源ノ角ゴシック Code JP R" :height 130)
 
 ;; 半角と全角の比を1:2に
 (add-to-list 'face-font-rescale-alist
              '(".*Code JP.*" . 1.2))
 
 ;; Color theme
-(el-get-bundle! color-theme)
-(el-get-bundle! color-theme-solarized)
-
-;; 背景色の設定
-;; http://d.hatena.ne.jp/kei10in/20101101/1288617632
-(custom-set-faces
- '(default ((t
-             (:background "black" :foreground "#55FF55")
-             )))
- '(cursor ((((class color)
-             (background dark))
-            (:background "#00AA00")))
-          (((class color)
-            (background light))
-           (:background "#999999"))
-          (t ())
-          ))
-
-;; 種類ごとの色
-;(add-hook 'font-lock-mode-hook
-;  '(lambda ()
-;    ;(set-face-foreground 'font-lock-comment-face "chocolate1")
-;    (set-face-foreground 'font-lock-comment-face "#f69933")  ; 微調整
-;    (set-face-foreground 'font-lock-string-face "LightSalmon")
-;    ;(set-face-foreground 'font-lock-keyword-face "Cyan1")
-;    (set-face-foreground 'font-lock-keyword-face "#66e6e6")  ; 微調整
-;    (set-face-foreground 'font-lock-builtin-face "LightSteelBlue")
-;    (set-face-foreground 'font-lock-function-name-face "LightSkyBlue")
-;    (set-face-foreground 'font-lock-variable-name-face "LightGoldenrod")
-;    (set-face-foreground 'font-lock-type-face "PaleGreen")
-;    (set-face-foreground 'font-lock-constant-face "Aquamarine")
-;    (set-face-foreground 'font-lock-warning-face "Pink")
-;  )
-;)
+(set-frame-parameter nil 'background-mode 'dark)
+(set-terminal-parameter nil 'background-mode 'dark)
+(el-get-bundle color-theme-solarized)
+(load-theme 'solarized t)
+(setq solarized-termcolors 256)
 
 ;; paren-mode : 対応する括弧を強調して表示する
 (setq show-paren-dely 0)                ;表示までの秒数．初期値は0.125
 (show-paren-mode t)                     ;有効化
 ;; paren のスタイル : expression は括弧内も強調表示
 ;; (setq show-paren-style 'expression)
-;; (setq show-paren-style 'mixed)
-(setq show-paren-style 'parenthesis)
+(setq show-paren-style 'mixed)
+;;(setq show-paren-style 'parenthesis)
 
 ;; 文字の折り返し
 ;; http://www.bookshelf.jp/soft/meadow_15.html#SEC132
