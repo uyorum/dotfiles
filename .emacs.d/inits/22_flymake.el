5f022b946ca74d6a938f673f3aa29405a77310a2
uyorum <uyorum.pub@gmail.com>
2016-02-03 21:39:56 +0900

    Emacs: flymake-cursor

--- .emacs.d/inits/22_flymake.el
+++ .emacs.d/inits/22_flymake.el
@@ -1,3 +1,8 @@
 (require 'flymake)
+(el-get-bundle! flymake-cursor in illusori/emacs-flymake-cursor)
+(setq flymake-cursor-error-display-delay 0.9)
+(setq flymake-cursor-number-of-errors-to-display 1)
+(setq flymake-cursor-auto-enable t)
+
 (el-get-bundle! flymake-ruby)
 (el-get-bundle! go-flymake)
