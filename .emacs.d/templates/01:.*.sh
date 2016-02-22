ac0d22da1b68be030c6d9caf45b4a251704f99fa
uyorum <uyorum.pub@gmail.com>
2016-02-22 13:09:45 +0900

    Emacs: fixed template for *.sh

--- .emacs.d/templates/01:.*.sh
+++ .emacs.d/templates/01:.*.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-readonly PROGNAME=$(basename $0)
-readonly PROGDIR=$(readlink -m $(dirname $0))
+readonly PROGNAME=$(basename \$0)
+readonly PROGDIR=$(readlink -m $(dirname \$0))
 
 usage() {
   cat <<HELP
 NAME:
-  $PROGNAME -- {one sentence description}
+  $PROGNAME - {one sentence description}
 
 SYNOPSIS:
   $PROGNAME [-h|--help]
@@ -41,27 +41,27 @@ main() {
         break
         ;;
       -*)
-        echo "$PROGNAME: illegal option -- '$(echo $1 | sed 's/^-*//')'" 1>&2
+        echo "$PROGNAME: illegal option -- '$(echo \$1 | sed 's/^-*//')'" 1>&2
         exit 1
         ;;
       *)
-        if [[ ! -z "$1" ]] && [[ ! "$1" =~ ^-+ ]]; then
-          args+=( "$1" )
+        if [[ ! -z "\$1" ]] && [[ ! "\$1" =~ ^-+ ]]; then
+          args+=( "\$1" )
           shift 1
         fi
-        ;; 
+        ;;
     esac
   done
 
   local required_args=0
   # Check number of arguments
-  if [ ! $required_args == 0 ] && [ "${args[$(expr $required_args - 1)]}" == "" ]; then
+  if [ ! $required_args == 0 ] && [ "\${args[$(expr $required_args - 1)]}" == "" ]; then
     echo "$PROGNAME: too few arguments" 1>&2
     echo "Try '$PROGNAME --help' for more information." 1>&2
     exit 1
   fi
 
-  # Arguments are in ${args[@]}
+  # Arguments are in \${args[@]}
 }
 
 main "$@"
