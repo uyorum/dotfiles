f3ae18badb80d60a6d852337abd589a4b71334db
uyorum <uyorum.pub@gmail.com>
2016-03-03 21:49:21 +0900

    Emacs: use Zeal also in Linux

---
+++ .emacs.d/inits/89_zeal.el
@@ -0,0 +1,9 @@
+(cond ((executable-find "zeal")
+       (el-get-bundle! jinzhu/zeal-at-point)
+       (global-set-key (kbd "C-c d") 'zeal-at-point)
+
+       (add-to-list 'zeal-at-point-mode-alist '(python-mode . "python"))
+       (add-to-list 'zeal-at-point-mode-alist '(ruby-mode . "ruby"))
+       (add-to-list 'zeal-at-point-mode-alist '(go-mode . "go"))
+       (add-to-list 'zeal-at-point-mode-alist '(shell-script-mode . "bash"))
+       ))
