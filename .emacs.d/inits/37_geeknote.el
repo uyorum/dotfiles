6a5e08839455363255156a88cf8a61f65fff5369
uyorum <uyorum.pub@gmail.com>
2016-03-10 23:08:19 +0900

    Emacs: geeknote-mode

---
+++ .emacs.d/inits/37_geeknote.el
@@ -0,0 +1,8 @@
+(el-get-bundle! geeknote in avendael/emacs-geeknote)
+
+(global-set-key (kbd "C-c g c") 'geeknote-create)
+(global-set-key (kbd "C-c g e") 'geeknote-edit)
+(global-set-key (kbd "C-c g f") 'geeknote-find)
+(global-set-key (kbd "C-c g s") 'geeknote-show)
+(global-set-key (kbd "C-c g r") 'geeknote-remove)
+(global-set-key (kbd "C-c g m") 'geeknote-move)
