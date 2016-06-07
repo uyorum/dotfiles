66010a2d49f20b0eb91b75cb0ee66fe60a567e3f
uyorum <uyorum.pub@gmail.com>
2016-06-07 22:16:12 +0900

    fix

--- .zsh/inits/10_util.zsh
+++ .zsh/inits/10_util.zsh
@@ -34,7 +34,7 @@ setopt share_history
 #
 # Others
 #
-# auto change directory↲
+# auto change directory
 setopt auto_cd
 
 # auto directory pushd that you can get dirs list by cd -[tab]
--- .zsh/inits/21_golang.zsh
+++
@@ -1,4 +0,0 @@
-in-path -q peco || go get github.com/peco/peco/cmd/peco
-in-path -q ghq || go get github.com/motemen/ghq
-in-path -q goflymake || go get -u github.com/dougm/goflymake
-in-path -q gocode || go get -u github.com/nsf/gocode
