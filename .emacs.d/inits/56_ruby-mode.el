d45610f33e7fadbe2775090b275febf04f932ff5
uyorum <uyorum.pub@gmail.com>
2016-08-16 22:56:10 +0900

    Emacs: Use ruby-mode intead of enh-ruby-mode
    It fails to install

--- .emacs.d/inits/56_ruby-mode.el
+++ .emacs.d/inits/56_ruby-mode.el
@@ -1,14 +1,7 @@
-(el-get-bundle! enh-ruby-mode in Enhanced-Ruby-Mode)
-(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("Capfile$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile$" . enh-ruby-mode))
-
+;; Use ruby-mode
 (el-get-bundle! flymake-ruby)
-(add-hook 'enh-ruby-mode-hook 'flymake-ruby-load)
 (add-hook 'ruby-mode-hook 'flymake-ruby-load)
 
 (el-get-bundle! rspec-mode)
 ;; load snippets by yasnippet
-(add-hook 'enh-ruby-mode-hook 'rspec-mode)
 (eval-after-load 'rspec-mode '(rspec-install-snippets))
