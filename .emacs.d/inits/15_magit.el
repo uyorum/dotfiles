99645ede6b8a0db805768bb052ad9fa79760233b
uyorum <uyorum.pub@gmail.com>
2016-10-16 20:18:28 +0900

    Emacs: Add magit dependancies

--- .emacs.d/inits/15_magit.el
+++ .emacs.d/inits/15_magit.el
@@ -1,4 +1,7 @@
 (el-get-bundle dash)
-(el-get-bundle! magit)
 
+(el-get-bundle! with-editor)
+(setq with-editor-file-name-history-exclude nil)
+
+(el-get-bundle! magit)
 (setq magit-auto-revert-mode nil)
