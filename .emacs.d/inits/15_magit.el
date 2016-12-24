83ea945ce7eee89e0d22918150976807127655a4
uyorum <uyorum.pub@gmail.com>
2016-12-24 09:28:29 +0900

    Emacs: Add key-bindings for magit

--- .emacs.d/inits/15_magit.el
+++ .emacs.d/inits/15_magit.el
@@ -5,3 +5,10 @@
 
 (el-get-bundle! magit)
 (setq magit-auto-revert-mode nil)
+
+(global-set-key (kbd "C-c g s") 'magit-status)
+(global-set-key (kbd "C-c g o") 'magit-checkout)
+(global-set-key (kbd "C-c g l") 'magit-pull)
+(global-set-key (kbd "C-c g p") 'magit-push)
+(global-set-key (kbd "C-c g m") 'magit-merge)
+(global-set-key (kbd "C-c g r") 'magit-cherry)
