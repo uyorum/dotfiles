928023e73eaa3731d7d5090f400130c2170bafc8
uyorum <uyorum.pub@gmail.com>
2017-01-24 22:07:44 +0900

    Emacs: Update auto-complete

--- .emacs.d/inits/31_auto-complete.el
+++ .emacs.d/inits/31_auto-complete.el
@@ -1,7 +1,17 @@
 (el-get-bundle! auto-complete-config in auto-complete)
 
-(global-auto-complete-mode 1)
+(setq ac-comphist-file (concat my:dir-dot-emacs-local "ac-comphist.dat"))
+
+(global-auto-complete-mode t)
 ;; emacs-lisp-mode でEmacs Lisp シンボルも補完
 (defun emacs-lisp-ac-setup ()
   (setq ac-sources '(ac-source-words-in-same-mode-buffers ac-source-symbols)))
 (add-hook 'emacs-lisp-mode-hook 'emacs-lisp-ac-setup)
+
+(define-key ac-completing-map (kbd "C-n") 'ac-next)
+(define-key ac-completing-map (kbd "C-p") 'ac-previous)
+
+;; 一つしか候補がない時にTAB(ac-expand)すると補完を完了(ac-complete)する
+;; 次候補/前候補を選んだ後にTAB(ac-expand)すると補完を完了(ac-complete)する
+;; 補完後にメニューを自動的に非表示にする
+(setq ac-dwim t)
