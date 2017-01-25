c12c8b09ca22629d2eef720bd4356847d0bb8ea0
uyorum <uyorum.pub@gmail.com>
2017-01-25 23:45:42 +0900

    Emacs: update magit config

--- .emacs.d/inits/15_magit.el
+++ .emacs.d/inits/15_magit.el
@@ -4,11 +4,13 @@
 (setq with-editor-file-name-history-exclude nil)
 
 (el-get-bundle! magit)
-(setq magit-auto-revert-mode nil)
+(setq magit-auto-revert-mode 1)
 
 (global-set-key (kbd "C-c g s") 'magit-status)
 (global-set-key (kbd "C-c g o") 'magit-checkout)
-(global-set-key (kbd "C-c g l") 'magit-pull)
-(global-set-key (kbd "C-c g p") 'magit-push)
+(global-set-key (kbd "C-c g p") 'magit-pull)
+(global-set-key (kbd "C-c g P") 'magit-push)
 (global-set-key (kbd "C-c g m") 'magit-merge)
 (global-set-key (kbd "C-c g r") 'magit-cherry)
+(global-set-key (kbd "C-c g l") 'magit-log-all)
+(global-set-key (kbd "C-c g L") 'magit-log-all-branches)
