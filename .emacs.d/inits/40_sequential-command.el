0535835d455cc8e4af9c66f32e4a7787f5ac957f
uyorum <uyorum.pub@gmail.com>
2017-01-25 23:50:47 +0900

    Emacs: seq-home

--- .emacs.d/inits/40_sequential-command.el
+++ .emacs.d/inits/40_sequential-command.el
@@ -3,22 +3,12 @@
 
 (sequential-command-setup-keys)
 
-(defun beginning-of-visual-indented-line (current-point)
-  "インデント文字を飛ばした行頭に戻る。ただし、ポイントから行頭までの間にインデント文字しかない場合は、行頭に戻る。"
-  (interactive "d")
-  (let ((vhead-pos (save-excursion (progn (beginning-of-visual-line) (point))))
-        (head-pos (save-excursion (progn (beginning-of-line) (point)))))
-    (cond
-     ;; 物理行の1行目にいる場合
-     ((eq vhead-pos head-pos)
-      (if (string-match
-           "^[ ¥t]+$"
-           (buffer-substring-no-properties vhead-pos current-point))
-          (beginning-of-visual-line)
-        (back-to-indentation)))
-     ;; 物理行の2行目以降の先頭にいる場合
-     ((eq vhead-pos current-point)
-      (backward-char)
-      (beginning-of-visual-indented-line (point)))
-     ;; 物理行の2行目以降の途中にいる場合
-     (t (beginning-of-visual-line)))))
+;; インデントを除いた行頭に移動
+(defun beginning-of-code ()
+  (interactive)
+  (beginning-of-line)
+  (skip-syntax-forward " " (line-end-position)))
+(define-sequential-command seq-home
+  beginning-of-code beginning-of-line beginning-of-buffer seq-return)
+
+(global-set-key (kbd "C-a") 'seq-home)
