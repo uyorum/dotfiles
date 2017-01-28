16e9879835a648136a1d7b21e97cf155a356c713
uyorum <uyorum.pub@gmail.com>
2017-01-28 23:13:37 +0900

    Emacs: directory name into vars

--- .emacs.d/inits/56_ruby-mode.el
+++ .emacs.d/inits/56_ruby-mode.el
@@ -19,33 +19,46 @@
 (eval-after-load 'rspec-mode '(rspec-install-snippets))
 
 ;; Ruby on Rails
-(global-set-key (kbd "C-c r v") '(lambda ()
-                                   (interactive)
-                                   (my:projectile-find-file-in-dir "app/views/")))
-(global-set-key (kbd "C-c r m") '(lambda ()
-                                   (interactive)
-                                   (my:projectile-find-file-in-dir "app/models/")))
-(global-set-key (kbd "C-c r c") '(lambda ()
-                                   (interactive)
-                                   (my:projectile-find-file-in-dir "app/controllers/")))
-(global-set-key (kbd "C-c r h") '(lambda ()
-                                   (interactive)
-                                   (my:projectile-find-file-in-dir "app/helpers/")))
-(global-set-key (kbd "C-c r s") '(lambda ()
-                                   (interactive)
-                                   (my:projectile-find-file-in-dir "spec/")))
-(global-set-key (kbd "C-c r t") '(lambda ()
-                                   (interactive)
-                                   (my:projectile-find-file-in-dir "test/")))
+(defvar my:projectile-find-prefix-views "app/views/")
+(defvar my:projectile-find-prefix-controllers "app/controllers/")
+(defvar my:projectile-find-prefix-models "app/models/")
+(defvar my:projectile-find-prefix-helpers "app/helpers/")
+(defvar my:projectile-find-prefix-spec "spec/")
+(defvar my:projectile-find-prefix-test "test/")
+
+(defun my:projectile-find-views ()
+  (interactive)
+  (my:projectile-find-file-in-dir my:projectile-find-prefix-views))
+(defun my:projectile-find-controllers ()
+  (interactive)
+  (my:projectile-find-file-in-dir my:projectile-find-prefix-controllers))
+(defun my:projectile-find-models ()
+  (interactive)
+  (my:projectile-find-file-in-dir my:projectile-find-prefix-models))
+(defun my:projectile-find-helpers ()
+  (interactive)
+  (my:projectile-find-file-in-dir my:projectile-find-prefix-helpers))
+(defun my:projectile-find-spec ()
+  (interactive)
+  (my:projectile-find-file-in-dir my:projectile-find-prefix-spec))
+(defun my:projectile-find-test ()
+  (interactive)
+  (my:projectile-find-file-in-dir my:projectile-find-prefix-test))
+
+(global-set-key (kbd "C-c r v") 'my:projectile-find-views)
+(global-set-key (kbd "C-c r m") 'my:projectile-find-controllers)
+(global-set-key (kbd "C-c r c") 'my:projectile-find-models)
+(global-set-key (kbd "C-c r h") 'my:projectile-find-helpers)
+(global-set-key (kbd "C-c r s") 'my:projectile-find-spec)
+(global-set-key (kbd "C-c r t") 'my:projectile-find-test)
 
 ;; 現在のControllerに対応するViewディレクトリを開く
-(global-set-key (kbd "C-c r V") '(lambda ()
-                                   (interactive)
-                                   (let ((my:dir (replace-regexp-in-string
-                                                  "_controller\.rb"
-                                                  "\/"
-                                                  (replace-regexp-in-string
-                                                   ".*\/app\/controllers"
-                                                   "app\/views"
-                                                   (buffer-file-name)))))
-                                     (my:projectile-find-file-in-dir my:dir))))
+(defun my:projectile-find-views-from-controllers ()
+  (interactive)
+  (let ((my:dir (replace-regexp-in-string
+                 "_controller\.rb" "\/"
+                 (replace-regexp-in-string
+                  ".*\/app\/controllers" "app\/views"
+                  (buffer-file-name)))))
+    (my:projectile-find-file-in-dir my:dir)))
+(global-set-key (kbd "C-c r V") 'my:projectile-find-views-from-controllers)
