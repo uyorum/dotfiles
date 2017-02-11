36af94ebcbf99fb4c8d5bc297fa9a5fa83469181
uyorum <uyorum.pub@gmail.com>
2017-02-12 00:26:41 +0900

    Emacs: Fix web-mode configurations
    * indent base on ruby code
    * update apperence

--- .emacs.d/inits/59_web-mode.el
+++ .emacs.d/inits/59_web-mode.el
@@ -1,9 +1,33 @@
-(el-get-bundle! web-mode :type github :pkgname "fxbois/web-mode")
+(el-get-bundle! fxbois/web-mode)
 
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
-(setq web-mode-engines-alist '(("ruby" . "\\.erb\\'")))
+
+(setq web-mode-style-padding 2)
+(setq web-mode-script-padding 2)
+(setq web-mode-block-padding 2)
+
+(setq web-mode-enable-auto-pairing t)
+(setq web-mode-enable-css-colorization t)
+(setq web-mode-enable-block-face nil)
+(setq web-mode-enable-part-face t)
+(setq web-mode-enable-comment-keywords t)
+(setq web-mode-enable-heredoc-fontification t)
+
+;; <, > and />
+(set-face-foreground 'web-mode-html-tag-bracket-face "#839496")
+
+(setq web-mode-enable-current-element-highlight t)
+(custom-set-faces
+ '(web-mode-current-element-highlight-face ((t (:background "#073642" :foreground "#2aa198" :weight bold)))))
+(setq web-mode-enable-current-column-highlight nil)
