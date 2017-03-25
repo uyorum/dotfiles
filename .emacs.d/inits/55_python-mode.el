fd94b8f431aa308fed517376a9d82462c99e5c98
uyorum <uyorum.pub@gmail.com>
2017-03-25 22:40:48 +0900

    Emacs: fix flymake-pyflakes

--- .emacs.d/inits/55_python-mode.el
+++ .emacs.d/inits/55_python-mode.el
@@ -7,10 +7,7 @@
 (add-hook 'python-mode-hook 'flymake-find-file-hook)
 (defun flymake-pyflakes-init ()
   (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                     'flymake-create-temp-inplace))
-         (local-file (file-relative-name
-                      temp-file
-                      (file-name-directory buffer-file-name))))
-    (list (concat BIN "/pycheck") (list local-file))))
+                     'flymake-create-temp-inplace)))
+    (list (concat BIN "/pycheck") (list temp-file))))
 (add-to-list 'flymake-allowed-file-name-masks
              '("\\.py\\'" flymake-pyflakes-init))
