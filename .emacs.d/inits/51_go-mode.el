3159a9295316fd21c54340adac18db3c6fe1adc2
uyorum <uyorum.pub@gmail.com>
2017-05-26 20:25:24 +0900

    Emacs: fixed go-mode
    * remove config for godef (use imenu instead of it)
    * remove go-remove-unused-imports and go-goto-imports (unneeded if using
      goimports)
    * and other configures are not needed

--- .emacs.d/inits/51_go-mode.el
+++ .emacs.d/inits/51_go-mode.el
@@ -1,21 +1,13 @@
 (el-get-bundle! go-mode)
 (el-get-bundle! go-flymake)
+(el-get-bundle! go-autocomplete)
 
 ;; need some go libraries
 ;; `go get -u github.com/dougm/goflymake`
 ;; `go get -u github.com/nsf/gocode`
 ;; `go get -u golang.org/x/tools/cmd/goimports`
 
-(el-get-bundle! go-autocomplete)
-
-(add-hook 'go-mode-hook
-          '(lambda()
-             (setq c-basic-offset 4)
-             (setq indent-tabs-mode t)
-             (local-set-key (kbd "M-.") 'godef-jump)
-             (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
-             (local-set-key (kbd "C-c i") 'go-goto-imports)
-             (local-set-key (kbd "C-c g") 'godoc)))
+(define-key go-mode-map (kbd "C-c C-d") 'godoc)
 
 ;; run gofmt before save
 (add-hook 'before-save-hook 'gofmt-before-save)
