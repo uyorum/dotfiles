9e39cba47055c45f1ae1325d7c2d364cc7ddd2bb
uyorum <uyorum.pub@gmail.com>
2017-06-10 19:51:11 +0900

    Emacs: add go-lint plugin
    * I don't make it run automaticaly yet

--- .emacs.d/inits/51_go-mode.el
+++ .emacs.d/inits/51_go-mode.el
@@ -1,11 +1,13 @@
 (el-get-bundle! go-mode)
 (el-get-bundle! go-flymake)
 (el-get-bundle! go-autocomplete)
+(el-get-bundle! golint in go-lint)
 
 ;; need some go libraries
 ;; `go get -u github.com/dougm/goflymake`
 ;; `go get -u github.com/nsf/gocode`
 ;; `go get -u golang.org/x/tools/cmd/goimports`
+;; `go get -u github.com/golang/lint`
 
 (define-key go-mode-map (kbd "C-c C-d") 'godoc)
 
