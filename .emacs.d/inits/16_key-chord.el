ca382819fb949297c57961c04a1590302a7d2fcb
uyorum <uyorum.pub@gmail.com>
2017-09-02 10:20:17 +0900

    Emacs: Tuned key-chord up

--- .emacs.d/inits/16_key-chord.el
+++ .emacs.d/inits/16_key-chord.el
@@ -1,11 +1,9 @@
 (el-get-bundle! key-chord)
 
 ;; 同時押しの許容誤差
-(setq key-chord-two-keys-delay 0.06)
+(setq key-chord-two-keys-delay 0.02)
 (key-chord-mode 1)
 ;; rf: recentf-open-files
 (key-chord-define-global "rf" 'recentf-open-files)
 ;; jk: toggle view-mode
 (key-chord-define-global "jk" 'view-mode)
-;; kc: toggle key chord
-(key-chord-define-global "kc" 'key-chord-mode)
