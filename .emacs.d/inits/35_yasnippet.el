64b15ee7aafba96fd7d15800f2618227a670a713
uyorum <uyorum.pub@gmail.com>
2017-09-03 15:29:54 +0900

    Emacs: Update yasnippet
    * Added snippets for web-mode (including js)
    * Added `.yas-parents` file instead of sym link

--- .emacs.d/inits/35_yasnippet.el
+++ .emacs.d/inits/35_yasnippet.el
@@ -2,3 +2,9 @@
 
 (setq yas-snippet-dirs (concat user-emacs-directory "snippets"))
 (yas-global-mode 1)
+
+(define-key yas-minor-mode-map (kbd "C-c s n") 'yas-new-snippet)
+(define-key yas-minor-mode-map (kbd "C-c s i") 'yas-insert-snippet)
+(define-key yas-minor-mode-map (kbd "C-c s v") 'yas-visit-snippet-file)
+(define-key yas-minor-mode-map (kbd "C-c s d") 'yas-describe-table)
+(define-key yas-minor-mode-map (kbd "C-c s l") 'yas-reload-all)
